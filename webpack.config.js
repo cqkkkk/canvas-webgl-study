8dbf4069da6e5c095b6476b8f5b9f90d2e41e17d
swnb <1786718956@qq.com>
2017-09-29 17:36:14 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+    entry: {
+        index: path.join(__dirname, 'index.js')
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+};
